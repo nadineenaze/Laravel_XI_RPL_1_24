2b289b479a4e56a0a889a7f8215f5e2202bb68b8
nadineenaze <nadineenaze@gmail.com>
2026-09-09 19:35:12 +0700

    update halaman

---
+++ resources/views/halaman_satu.blade.php
@@ -0,0 +1,11 @@
+<html>
+
+<head>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1 class="text-3xl font-bold underline text-purple-400">NAWWA</h1>
+    <p>halaman ini menggunakan view</p>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\HalamanController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+ Route::get('/', function () {
+     return view('welcome');
+ });
+ Route::get('/', function () {
+     return "Selamat Datang";
+ });
+ Route::get("/hello", function () {
+     return "Ini adalah halaman hello";
+ });
+
+Route::get("/", [HalamanController::class, "halamanPertama"]);
